4d895683e11c8fec1e9a7a5e214e89dbf8fc70b5
younggyun lee <younggyun12@hotmail.com>
2025-04-04 09:18:58 +0900

    Initial commit

---
+++ src/test/java/com/flashpay/flashpay/FlashpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashpay.flashpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
